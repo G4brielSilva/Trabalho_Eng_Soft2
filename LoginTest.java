8c5874be01c16b39689ba434a659ac0a19234288
Neri Allemand <allemandneri@gmail.com>
2023-10-02 20:33:23 -0300

    adicionado testes para senha

--- LoginTest.java
+++ LoginTest.java
@@ -13,6 +13,44 @@ public class LoginTest {
         return !userIsEmpty(user) && !userHasInvalidCharacters(user);
     }
 
+    public static Boolean PasswordHasLessThan8Characters(String password) {
+        return password.length()<8;
+    }
+    
+    public static Boolean PasswordHasUppercase(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isUpperCase(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static Boolean PasswordHasNumber(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static Boolean PasswordHasSpecialChar(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (!Character.isDigit(c) && !Character.isLetter(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static Boolean PasswordIsValid(String password) {
+        return !PasswordHasLessThan8Characters(password) && PasswordHasUppercase(password) && PasswordHasNumber(password) && PasswordHasSpecialChar(password);
+    }
+    
     @Test
     public void test() {
         // User is valid
@@ -20,38 +58,47 @@ public class LoginTest {
         Assert.assertFalse(userIsValid("invalid-username"));
 
         Assert.assertTrue(userIsValid("validUserName"));
+        
+        Assert.assertFalse(PasswordIsValid("A4!")); //Menos de 8 caracteres
+        Assert.assertFalse(PasswordIsValid("@2345678")); //Sem letra maiuscula
+        Assert.assertFalse(PasswordIsValid("Abcdefg@")); //Sem numero
+        Assert.assertFalse(PasswordIsValid("A2345678")); //Sem caracter especial
+        
+        Assert.assertTrue(PasswordIsValid("A@345678"));
     }
 }
 
-public class VerificarSenha {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a senha: ");
-        String senha = scanner.nextLine();
 
-        if (verificarSenha(senha)) {
-            System.out.println("Senha válida!");
-        } else {
-            System.out.println("Senha inválida!");
-        }
-    }
 
-    public static boolean verificarSenha(String senha) {
-        // Verificar o comprimento mínimo
-        if (senha.length() < 8) {
-            return false;
-        }
-
-        // Verificar se há pelo menos 1 caractere maiúsculo
-        boolean temMaiusculo = false;
-        for (int i = 0; i < senha.length(); i++) {
-            char c = senha.charAt(i);
-            if (Character.isUpperCase(c)) {
-                temMaiusculo = true;
-                break;
-            }
-        }
-
-        return temMaiusculo;
-    }
-}
+//public class VerificarSenha {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Digite a senha: ");
+//        String senha = scanner.nextLine();
+//
+//        if (verificarSenha(senha)) {
+//            System.out.println("Senha válida!");
+//        } else {
+//            System.out.println("Senha inválida!");
+//        }
+//    }
+//
+//    public static boolean verificarSenha(String senha) {
+//        // Verificar o comprimento mínimo
+//        if (senha.length() < 8) {
+//            return false;
+//        }
+//
+//       // Verificar se há pelo menos 1 caractere maiúsculo
+//        boolean temMaiusculo = false;
+//        for (int i = 0; i < senha.length(); i++) {
+//            char c = senha.charAt(i);
+//            if (Character.isUpperCase(c)) {
+//                temMaiusculo = true;
+//                break;
+//            }
+//        }
+//
+//        return temMaiusculo;
+//    }
+//}
