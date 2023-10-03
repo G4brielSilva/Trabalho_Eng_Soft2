24e8866e52dc9e6654eb3bf59b3a5829a6676a94
G4brielSilva <gabrielsilvacarvalho@id.uff.br>
2023-10-03 11:35:03 -0300

    refactor: removendo código comentado

--- LoginTest.java
+++ LoginTest.java
@@ -67,38 +67,3 @@ public class LoginTest {
         Assert.assertTrue(PasswordIsValid("A@345678"));
     }
 }
-
-
-
-//public class VerificarSenha {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Digite a senha: ");
-//        String senha = scanner.nextLine();
-//
-//        if (verificarSenha(senha)) {
-//            System.out.println("Senha válida!");
-//        } else {
-//            System.out.println("Senha inválida!");
-//        }
-//    }
-//
-//    public static boolean verificarSenha(String senha) {
-//        // Verificar o comprimento mínimo
-//        if (senha.length() < 8) {
-//            return false;
-//        }
-//
-//       // Verificar se há pelo menos 1 caractere maiúsculo
-//        boolean temMaiusculo = false;
-//        for (int i = 0; i < senha.length(); i++) {
-//            char c = senha.charAt(i);
-//            if (Character.isUpperCase(c)) {
-//                temMaiusculo = true;
-//                break;
-//            }
-//        }
-//
-//        return temMaiusculo;
-//    }
-//}
